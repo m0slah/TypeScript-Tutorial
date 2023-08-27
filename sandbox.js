ea7fab627b9d148f14123f9c5814924349fe2bb6
m0slah <muhammedslahqadir@gmail.com>
2023-08-27 15:33:43 +0300

    Type Basics

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,20 @@
-var character = "Slah";
-console.log(character);
-var inputs = document.querySelectorAll("input");
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
+// const character = "Slah";
+// console.log(character);
+// const inputs = document.querySelectorAll("input");
+// console.log(inputs);
+// inputs.forEach((input) => {
+//   console.log(input);
+// });
+var character = "MO";
+var age = 22;
+var isBlackBelt = true;
+// character = 22; //Type 'number' is not assignable to type 'string'
+character = "Mohammed";
+// age = "20";  //Type 'string' is not assignable to type 'number'.
+age = 20;
+// isBlackBelt="False" //Type 'string' is not assignable to type 'boolean'.
+isBlackBelt = false;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(3));
--- sandbox.ts
+++ sandbox.ts
@@ -1,12 +1,30 @@
-const character = "Slah";
+// const character = "Slah";
 
-console.log(character);
+// console.log(character);
 
-const inputs = document.querySelectorAll("input");
+// const inputs = document.querySelectorAll("input");
 
-console.log(inputs);
+// console.log(inputs);
 
-inputs.forEach((input) => {
-  console.log(input);
-});
+// inputs.forEach((input) => {
+//   console.log(input);
+// });
 
+let character = "MO";
+let age = 22;
+let isBlackBelt = true;
+
+// character = 22; //Type 'number' is not assignable to type 'string'
+character = "Mohammed";
+
+// age = "20";  //Type 'string' is not assignable to type 'number'.
+age = 20;
+
+// isBlackBelt="False" //Type 'string' is not assignable to type 'boolean'.
+isBlackBelt = false;
+
+const circ = (diameter: string) => {
+  return diameter * Math.PI;
+};
+
+console.log(circ(3));
