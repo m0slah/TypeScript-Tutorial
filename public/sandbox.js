dc3d640d9842d91b468e89eb7454498d9669591a
m0slah <muhammedslahqadir@gmail.com>
2023-08-28 10:21:41 +0300

    Function Basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("sandbox");
+let greet;
+greet = () => {
+    console.log("hello");
+};
+const add = (a, b, c) => {
+    console.log(a + b); // return type is inferred as 'number' by TypeScript compiler when no explicit return statement exists in the function body
+};
+add(5, 8);
+const minus = (a, b) => {
+    return a - b;
+};
+let result = minus(8, 4);
+console.log(result);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,19 @@
-console.log("sandbox");
+let greet: Function;
+
+greet = () => {
+  console.log("hello");
+};
+
+const add = (a: number, b: number, c?: number | string): void => {
+  console.log(a + b); // return type is inferred as 'number' by TypeScript compiler when no explicit return statement exists in the function body
+};
+
+add(5, 8);
+
+const minus = (a: number, b: number) => {
+  return a - b;
+};
+
+let result = minus(8, 4);
+
+console.log(result);
