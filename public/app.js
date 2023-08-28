46400e332dc04e604b1850cf86117c981df17240
m0slah <muhammedslahqadir@gmail.com>
2023-08-28 13:04:24 +0300

    classes

--- public/app.js
+++ public/app.js
@@ -1,18 +1,31 @@
 "use strict";
-const anchor = document.querySelector('a');
-if (anchor) {
-    console.log(anchor.href);
+class Invoice {
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    format() {
+        return `${this.client} owns $ ${this.amount} for ${this.details}`;
+    }
 }
-console.log(anchor.href);
+const invoiceOne = new Invoice("mario", "work on the mario website", 250);
+const invoiceTwo = new Invoice("john", "work on the market systme", 350);
+let invoices = [];
+invoices.push(invoiceOne);
+invoices.push(invoiceTwo);
+invoiceOne.client = "mO";
+invoiceTwo.amount = 200;
+console.log(invoices);
 //const form = document.querySelector('form')!;
-const form = document.querySelector('.new-item-form');
+const form = document.querySelector(".new-item-form");
 console.log(form.children);
 // inputs
-const type = document.querySelector('#type');
-const tofrom = document.querySelector('#tofrom');
-const details = document.querySelector('#details');
-const amount = document.querySelector('#amount');
-form.addEventListener('submit', (e) => {
+const type = document.querySelector("#type");
+const tofrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
