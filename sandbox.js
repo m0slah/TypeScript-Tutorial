668f107332778673ea48463e1b4788a0b9ab82ba
m0slah <muhammedslahqadir@gmail.com>
2023-08-28 09:07:40 +0300

    Dynamic (any) Types

--- sandbox.js
+++ sandbox.js
@@ -1,39 +1,17 @@
-// explicit types
-var character;
-var age;
-var isLoggined;
-// character = 22; //Type 'number' is not assignable to type 'string'.
-character = "name";
-// age = "number"; //Type 'string' is not assignable to type 'number'.
-age = 22;
-// isLoggined="flase" //Type 'string' is not assignable to type 'boolean'.
-isLoggined = true;
-// arrays
-var names;
-var numbers;
-var booleans;
-// names = [1, 2, 3, 4]; //Type 'number' is not assignable to type 'string'.
-names = ["abc", "def"];
-// numbers = ["abc","def","ijk"]; //Type 'string' is not assignable to type 'number'.
-numbers = [1, 2, 4, 5];
-// booleans=["true" , "false" ]; //Type 'string' is not assignable to type 'boolean'.
-booleans = [true, false, true];
-// union types
+var age = 25;
+age = true;
+age = "test";
+age = { name: "mice" };
 var mixed = [];
-mixed.push("hello");
-mixed.push(12);
+mixed.push("test");
+mixed.push(11);
 mixed.push(false);
-console.log(mixed);
-var uid;
-uid = "test";
-uid = 10;
-// uid = false; //Type 'boolean' is not assignable to type 'string | number'.
-// objects
-var ninjaOne;
-ninjaOne = { name: "test", age: 10, isLoggined: false };
-var ninjaTwo;
-ninjaTwo = {
-    name: "mario",
-    age: 10,
-    haveAccount: false,
+var ninja;
+ninja = {
+    name: "test",
+    age: 22,
+};
+ninja = {
+    name: 22,
+    age: "test",
 };
--- sandbox.ts
+++ sandbox.ts
@@ -1,59 +1,23 @@
-// explicit types
+let age: any = 25;
 
-let character: string;
-let age: number;
-let isLoggined: boolean;
+age = true;
+age = "test";
+age = { name: "mice" };
 
-// character = 22; //Type 'number' is not assignable to type 'string'.
-character = "name";
+let mixed: any[] = [];
 
-// age = "number"; //Type 'string' is not assignable to type 'number'.
-age = 22;
-
-// isLoggined="flase" //Type 'string' is not assignable to type 'boolean'.
-isLoggined = true;
-
-// arrays
-let names: string[];
-let numbers: number[];
-let booleans: boolean[];
-// names = [1, 2, 3, 4]; //Type 'number' is not assignable to type 'string'.
-names = ["abc", "def"];
-
-// numbers = ["abc","def","ijk"]; //Type 'string' is not assignable to type 'number'.
-numbers = [1, 2, 4, 5];
-
-// booleans=["true" , "false" ]; //Type 'string' is not assignable to type 'boolean'.
-booleans = [true, false, true];
-
-// union types
-let mixed: (string | number | boolean)[] = [];
-
-mixed.push("hello");
-mixed.push(12);
+mixed.push("test");
+mixed.push(11);
 mixed.push(false);
 
-console.log(mixed);
+let ninja: { name: any; age: any };
 
-let uid: string | number;
-
-uid = "test";
-uid = 10;
-// uid = false; //Type 'boolean' is not assignable to type 'string | number'.
-
-// objects
-let ninjaOne: object;
-
-ninjaOne = { name: "test", age: 10, isLoggined: false };
-
-let ninjaTwo: {
-  name: string;
-  age: number;
-  haveAccount: boolean;
+ninja = {
+  name: "test",
+  age: 22,
 };
 
-ninjaTwo = {
-  name: "mario",
-  age: 10,
-  haveAccount: false,
+ninja = {
+  name: 22,
+  age: "test",
 };
